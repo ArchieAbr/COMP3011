e5b48a438aa7e1eaf053e08d48a4fb7448e6ad25
ArchieAbr <archiepabraham@gmail.com>
2026-02-18 18:48:40 +0000

    Initialise UrbanPulse FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from .database import engine, Base, get_db
+from .models import Property, CrimeStat, MarketTrend # Imports from __init__.py
 
-app = FastAPI(title="UrbanPulse API")
+app = FastAPI(title="UrbanPulse 360 API")
+
+# This triggers the table creation in Azure
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
-    return {"status": "UrbanPulse is Online"}
+    return {"status": "UrbanPulse modular structure is active"}
